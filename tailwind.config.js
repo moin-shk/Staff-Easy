42f0cbd7bcd5515fc6a292ab1d011b74f0295ef4
Nebula580 <evandiplacido@outlook.com>
2025-04-09 12:06:57 -0600

    Added navabr

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./staffeasy-frontend/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
